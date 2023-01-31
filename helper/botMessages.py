3ccca1e9b5b7e06a2150a5025251c4e8395c3ec5
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-01 00:07:55 +0200

    Update botMessages.py

--- helper/botMessages.py
+++ helper/botMessages.py
@@ -5,7 +5,7 @@
 class BotMessage(object):
     common_text = "\n\n<u>If you are facing any problem, so report at @AJPyroVerseGroup</u>"
     help_msg = f"<i>To use me, Just Send me any direct downloading link, and I will send you the file as telegram file.</i>{common_text}"
-    start_msg = f"<b>Hi, I am URL_UploaderBot Created by @AJPyroVerse and My Developer is @AJTimePyro.</b>\n{help_msg}"
+    start_msg = f"<b>السلام عليكم أنا بوت لرفع الملفات , فقط أرسل رابط تحميل مباشر لملف و سأرفعه لك على التلجرام \n\n لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 \n\n لدعم استمرار المشروع هنا \n\n http://paypal.me/kelectronic89 \n\n تم تطويره بواسطة  @AJTimePyro"
     not_joined_community = f"<b>To use this bot, you need to Join our Channel and Group.</b>{common_text}"
     broadcast_failed = "<b>Broadcasting Message can`t be empty</b>"
     processing_url = "<i>Please wait while I am Processing File</i>"
