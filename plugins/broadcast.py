fd8bc606793a3e122c82876f444dc3466d734899
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-12 06:04:07 +0200

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -20,7 +20,7 @@ async def broadcast_handler(bot, update):
         #Extracting Broadcasting Message
         message = update.text.split('/broadcast ')[1]
     except IndexError:
-        await update.reply_text(BotMessage.broadcast_failed, parse_mode = 'html')
+        await update.reply_text(BotMessage.broadcast_failed, parse_mode =enums.ParseMode.HTML)
     except Exception as e:
         await bot.send_message(Config.OWNER_ID, line_number(fileName, e))
     else:
