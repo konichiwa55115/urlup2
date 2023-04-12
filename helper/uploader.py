4085b009e1a30dea32c2aba34d5bd92a69aefa81
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-12 06:07:54 +0200

    Update uploader.py

--- helper/uploader.py
+++ helper/uploader.py
@@ -28,7 +28,7 @@ class Upload:
 
         async def editMessage(progress_bar, percentage, completed, speed, remaining):
             try:
-                self.old_msg = await self.bot.edit_message_text(self.userid, self.old_msg.message_id, f"<b>Now Uploading... !! Have patience... ⌛\n [{progress_bar}]\n📊Percentage: {percentage} %\n✅Completed: {completed} MB\n🚀Speed: {speed} MB/s\n⌚️Remaining Time: {remaining} seconds</b>", parse_mode = 'html')
+                self.old_msg = await self.bot.edit_message_text(self.userid, self.old_msg.message_id, f"<b>Now Uploading... !! Have patience... ⌛\n [{progress_bar}]\n📊Percentage: {percentage} %\n✅Completed: {completed} MB\n🚀Speed: {speed} MB/s\n⌚️Remaining Time: {remaining} seconds</b>", parse_mode = enums.ParseMode.HTML)
             except exceptions.bad_request_400.MessageNotModified:
                 pass
 
